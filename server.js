de28e8e11de6f7d7e23ad9dd5c1b1c3769e7d31a
Shawn Benny <sbenn9210@gmail.com>
2020-04-24 17:02:46 -0500

    Add put route for all owners

--- server.js
+++ server.js
@@ -1,52 +1,77 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+var express = require("express");
+var bodyParser = require("body-parser");
 
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 var owners = [
-    {
+  {
+    id: 1,
+    name: "Adam",
+    pets: [
+      {
         id: 1,
-        name: "Adam",
-        pets: [
-            {
-                id: 1,
-                name: "Vera",
-                type: "Dog"
-            },
-            {
-                id: 2,
-                name: "Felix",
-                type: "Cat"
-            }
-        ]
-    },
-    {
+        name: "Vera",
+        type: "Dog",
+      },
+      {
         id: 2,
-        name: "Kamilah",
-        pets: [
-            {
-                id: 1,
-                name: "Doug",
-                type: "Dog"
-            }
-        ]
-    }
+        name: "Felix",
+        type: "Cat",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Kamilah",
+    pets: [
+      {
+        id: 1,
+        name: "Doug",
+        type: "Dog",
+      },
+    ],
+  },
 ];
-
 
 // GET /api/owners
 
+app.get("/api/owners", (req, res) => {
+  res.send(owners);
+});
+
 // GET /api/owners/:id
+
+app.get("/api/owners/:id", (req, res) => {
+  let owner = owners.filter((person) => person.id == req.params.id);
+  res.send(owner);
+});
 
 // POST /api/owners
 
+app.post("/api/owners", (req, res) => {
+  console.log(req.body);
+  owners.push(req.body);
+  res.send(owners);
+});
+
 // PUT /api/owners/:id
 
+app.put("/api/owners/:id", (req, res) => {
+  let updatedOwner = req.body;
+  let updatedOwnersList = owners.map((owner) =>
+    owner.id == req.params.id ? (owners[owner] = updatedOwner) : owner
+  );
+  res.send(updatedOwnersList);
+});
 // DELETE /api/owners/:id
 
+app.delete("/api/owners/:id", (req, res) => {});
+
 // GET /api/owners/:id/pets
+
+app.get("/api/owners/:id/pets", (req, res) => {});
 
 // GET /api/owners/:id/pets/:petId
 
@@ -56,7 +81,6 @@ var owners = [
 
 // DELETE /api/owners/:id/pets/:petId
 
-
-app.listen(3000, function(){
-    console.log('Pets API is now listening on port 3000...');
-})
+app.listen(3000, function () {
+  console.log("Pets API is now listening on port 3000...");
+});
